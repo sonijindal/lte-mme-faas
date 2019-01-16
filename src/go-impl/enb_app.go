fee246c743ebe62457b591cd25ec4c9c36617314
sonika05 <sonika05@node.sj-ubuntu-pc.phantomnet-pg0.utah.cloudlab.us>
2019-01-16 12:57:34 -0700

    Capture time taken

--- src/go-impl/enb_app.go
+++ src/go-impl/enb_app.go
@@ -2,7 +2,7 @@
 
 // This is the UE/enb simulator
 //export GOPATH=$GOPATH:/users/sonika05/lte-enb-mme
-// go run enb.go <mme_ip> <num_ue>
+// go run enb.go <mme_ip> <num_ue> <async optional>
 package main
 
 import (
@@ -16,11 +16,21 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var mme_ip string
 var req_url string
 var async int
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var time_arr []time.Time
+var fd *os.File
 
 func main() {
 	args := os.Args[1:]
@@ -33,6 +43,14 @@ func main() {
 		async, _ = strconv.Atoi(args[2])
 	}
 	Ue_info_arr = make([]Ue_info, num_ue)
+	time_arr = make([]time.Time, num_ue)
+	file := "./test/results/app" + strconv.Itoa(async) + "-" + time.Now().Format("2006.01.02-15:04:05")
+	fmt.Println(file)
+	var err error
+	fd, err = os.Create(file)
+	check(err)
+	defer fd.Close()
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Start Time: ", time.Now())
 	if async == 1 {
 		req_url = "http://128.110.154.116:31112/async-function/mme-faas-go"
 		go send_request(num_ue)
@@ -76,7 +94,7 @@ func process_async_msg(w http.ResponseWriter, req *http.Request) {
 
 			auth_req := msg.Auth_req
 			id := auth_req.Enb_ue_s1ap_id
-			fmt.Printf("AUTH_REQ received, %d\n", id)
+			//fmt.Printf("AUTH_REQ received, %d\n", id)
 			Ue_info_arr[id].Message.Msg_type = AUTH_RES
 			Ue_info_arr[id].Message.Auth_res.Mme_ue_s1ap_id =
 				auth_req.Mme_ue_s1ap_id
@@ -90,7 +108,7 @@ func process_async_msg(w http.ResponseWriter, req *http.Request) {
 
 			sec_mode_command := msg.Sec_mode_command
 			id := sec_mode_command.Enb_ue_s1ap_id
-			fmt.Printf("SEC_MODE_COMMAND received, %d\n", id)
+			//fmt.Printf("SEC_MODE_COMMAND received, %d\n", id)
 			Ue_info_arr[id].Message.Msg_type = SEC_MODE_COMPLETE
 			Ue_info_arr[id].Message.Sec_mode_complete.Mme_ue_s1ap_id =
 				sec_mode_command.Mme_ue_s1ap_id
@@ -101,7 +119,10 @@ func process_async_msg(w http.ResponseWriter, req *http.Request) {
 			send_response(Ue_info_arr[id].Message)
 
 		case ATTACH_ACCEPT:
-			fmt.Printf("ATTACH_ACCEPT received, %d\n", msg.Attach_accept.Enb_ue_s1ap_id)
+			id := msg.Attach_accept.Enb_ue_s1ap_id
+			elapsed := time.Since(time_arr[id])
+			fd.WriteString(strconv.Itoa(int(id)) + ":" + elapsed.String() + "\n")
+			//fmt.Printf("ATTACH_ACCEPT received, %d\n", msg.Attach_accept.Enb_ue_s1ap_id)
 
 		default:
 			fmt.Printf("Unhandled message(%s) received\n", msg.Msg_type)
@@ -133,6 +154,7 @@ func process_msg(req *http.Response) {
 	var msg Message_union_t
 	err := json.Unmarshal(body_bytes, &msg)
 	if err != nil {
+		fmt.Printf("Message from mme %s, len:%d\n", body, len(body))
 		panic(err)
 	}
 
@@ -142,7 +164,7 @@ func process_msg(req *http.Response) {
 
 			auth_req := msg.Auth_req
 			id := auth_req.Enb_ue_s1ap_id
-			fmt.Printf("AUTH_REQ received, %d\n", id)
+			//fmt.Printf("AUTH_REQ received, %d\n", id)
 			Ue_info_arr[id].Message.Msg_type = AUTH_RES
 			Ue_info_arr[id].Message.Auth_res.Mme_ue_s1ap_id =
 				auth_req.Mme_ue_s1ap_id
@@ -156,7 +178,7 @@ func process_msg(req *http.Response) {
 
 			sec_mode_command := msg.Sec_mode_command
 			id := sec_mode_command.Enb_ue_s1ap_id
-			fmt.Printf("SEC_MODE_COMMAND received, %d\n", id)
+			//fmt.Printf("SEC_MODE_COMMAND received, %d\n", id)
 			Ue_info_arr[id].Message.Msg_type = SEC_MODE_COMPLETE
 			Ue_info_arr[id].Message.Sec_mode_complete.Mme_ue_s1ap_id =
 				sec_mode_command.Mme_ue_s1ap_id
@@ -167,7 +189,11 @@ func process_msg(req *http.Response) {
 			send_response(Ue_info_arr[id].Message)
 
 		case ATTACH_ACCEPT:
-			fmt.Printf("ATTACH_ACCEPT received, %d\n", msg.Attach_accept.Enb_ue_s1ap_id)
+			id := msg.Attach_accept.Enb_ue_s1ap_id
+			elapsed := time.Since(time_arr[id])
+			fd.WriteString(strconv.Itoa(int(id)) + ":" + elapsed.String() + "\n")
+			//fmt.Printf("ATTACH_ACCEPT received, %d\n", msg.Attach_accept.Enb_ue_s1ap_id)
+			fmt.Println("End: ", time.Now())
 
 		default:
 			fmt.Printf("Unhandled message(%s) received\n", msg.Msg_type)
@@ -180,13 +206,14 @@ func process_msg(req *http.Response) {
 func send_request(num_ue int) {
 	//req_url := "http://" + mme_ip + ":8002"
 	for i := 0; i < num_ue; i++ {
-		fmt.Printf("Sending ATTACH_REQ:%d\n", i)
+		//fmt.Printf("Sending ATTACH_REQ:%d\n", i)
 		Ue_info_arr[i].Message.Msg_type = ATTACH_REQ
 		Ue_info_arr[i].Message.Attach_req.Imsi = strconv.Itoa(i)
 		Ue_info_arr[i].Message.Attach_req.Enb_ue_s1ap_id = uint64(i)
 		Ue_info_arr[i].Message.Attach_req.Plmn_id = 1
 		Ue_info_arr[i].Message.Attach_req.Tai = 1
 		Ue_info_arr[i].Message.Attach_req.Net_cap = 1
+		time_arr[i] = time.Now()
 		send_response(Ue_info_arr[i].Message)
 		//form := new(bytes.Buffer)
 		// json.NewEncoder(form).Encode(msg)
@@ -209,13 +236,21 @@ func send_response(msg Message_union_t) {
 		req, _ := http.NewRequest("POST", req_url, form)
 		if async == 1 {
 			req.Header.Add("X-Callback-Url", "http://128.110.154.116:8001")
-			resp, _ := client.Do(req)
-			defer resp.Body.Close()
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Couldn't send req!")
+			} else {
+				fmt.Println(resp)
+			}
 		} else {
-			resp, _ := client.Do(req)
-			defer resp.Body.Close()
+			resp, err := client.Do(req)
+			if err == nil {
+				defer resp.Body.Close()
+				process_msg(resp)
+			} else {
+				fmt.Println("Couldn't send req!", err)
+			}
 			//resp, _ := http.Post(req_url, "application/json", form)
-			process_msg(resp)
 		}
 	}(req_url, form)
 
