c259230d9b069fb05fa0d6751cc0fc2e9e0bed5a
sonika05 <sonika05@node.sj-ubuntu-pc.phantomnet-pg0.utah.cloudlab.us>
2019-01-18 12:46:14 -0700

    Add dynamo support

---
+++ src/mme_faas_aws_dynamo/dynamo_access.go
@@ -0,0 +1,119 @@
+// dynamo_access.go
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Item struct {
+	Key  uint64  `json:"key"`
+	Info Ue_info `json:"info"`
+}
+
+// Declare a new DynamoDB instance. Note that this is safe for concurrent
+// use.
+var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-east-2"))
+
+func getItem(key int) (*Ue_info, error) {
+	// Prepare the input for the query.
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("ue_info"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				N: aws.String(strconv.Itoa(key)),
+			},
+		},
+	}
+
+	// Retrieve the item from DynamoDB. If no matching item is found
+	// return nil.
+	result, err := db.GetItem(input)
+	if err != nil {
+		return nil, err
+	}
+	if result.Item == nil {
+		return nil, nil
+	}
+
+	item := new(Ue_info)
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+
+	return item, err
+}
+func insert(id uint64, ue_info Ue_info) bool {
+	item := new(Item)
+	item.Key = id
+	item.Info = ue_info
+	av, err := dynamodbattribute.MarshalMap(item)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("ue_info"),
+	}
+
+	_, err = db.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return true
+}
+
+func get(id uint64) (Ue_info, error) {
+	//info := Ue_info{}
+	item := Item{}
+	var err error
+	err = nil
+	// Prepare the input for the query.
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("ue_info"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				N: aws.String(strconv.FormatUint(id, 10)),
+			},
+		},
+	}
+
+	// Retrieve the item from DynamoDB. If no matching item is found
+	// return nil.
+	result, err1 := db.GetItem(input)
+	if err1 != nil {
+		return item.Info, err1
+	}
+	if result.Item == nil {
+		return item.Info, nil
+	}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	return item.Info, err
+}
+
+func update(id uint64, ue_info Ue_info) bool {
+	item := new(Item)
+	item.Key = id
+	item.Info = ue_info
+	av, err := dynamodbattribute.MarshalMap(item)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("ue_info"),
+	}
+
+	_, err = db.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return true
+}
---
+++ src/mme_faas_aws_dynamo/mme_faas_aws_dynamo.go
@@ -0,0 +1,162 @@
+// mme.go
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	//	"handler/function/common"
+	"math/rand"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(Handle)
+}
+
+func Handle(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	res := HandleReq([]byte(request.Body))
+	return events.APIGatewayProxyResponse{
+		Body:       string(res[:]),
+		StatusCode: 200,
+	}, nil
+}
+
+// Handle a serverless request
+func HandleReq(req []byte) []byte {
+	res := process_event(req)
+	return res
+}
+func generate_mme_id() uint64 {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	return (uint64(r1.Uint32())<<32 + uint64(r1.Uint32()))
+}
+func process_event(req []byte) []byte {
+	var msg Message_union_t
+	json.Unmarshal(req, &msg)
+	switch msg.Msg_type {
+	case ATTACH_REQ:
+		//hss_sgw_stub(AUTH_INFO_REQ) //ignore ret
+
+		id := generate_mme_id()
+		ue_info := Ue_info{Ue_id: msg.Attach_req.Imsi, Tai: msg.Attach_req.Tai, Enb_ue_s1ap_id: msg.Attach_req.Enb_ue_s1ap_id,
+			Ue_state: IDLE, Plmn_id: msg.Attach_req.Plmn_id, Mme_ue_s1ap_id: id}
+		auth_req := build_auth_request(id, ue_info)
+		for insert(id, ue_info) != true {
+			id = generate_mme_id()
+			ue_info.Mme_ue_s1ap_id = id
+			ue_info.Message.Auth_req.Mme_ue_s1ap_id = id
+		}
+		var msg Message_union_t
+		err := json.Unmarshal(auth_req, &msg)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Printf("Sending response of size:%d\n", len(auth_req))
+		//fmt.Printf("ATTACH_REQ received, %d, mme id:%d\n", msg.Attach_req.Enb_ue_s1ap_id, id)
+		return auth_req
+
+	case AUTH_RES:
+		//generate nas keys
+
+		id := msg.Auth_res.Mme_ue_s1ap_id
+		//fmt.Printf("AUTH_RES received, %d, mme_ue_id:%d\n", msg.Auth_res.Enb_ue_s1ap_id, id)
+		ue_info, err := get(id)
+		if err != nil {
+			fmt.Println("Couldnt retrieve ue id:", id, err.Error())
+			break
+		}
+
+		sec_mode_command := build_sec_mode_command(id, ue_info)
+		return sec_mode_command
+
+	case SEC_MODE_COMPLETE:
+		//hss_sgw_stub(UPDATE_LOC_REQ)     //ignore ret
+		//hss_sgw_stub(CREATE_SESSION_REQ) //ignore ret
+
+		id := msg.Sec_mode_complete.Mme_ue_s1ap_id
+		//fmt.Printf("SEC_MODE_COMPLETE received, %d, mme_ue_id:%d\n", msg.Sec_mode_complete.Enb_ue_s1ap_id, id)
+		ue_info, err := get(id)
+		if err != nil {
+			fmt.Println("Couldnt retrieve ue id:", id, err.Error())
+			break
+		}
+		attach_accept := build_attach_accept(id, ue_info)
+
+		return attach_accept
+
+	default:
+		//hss_sgw_stub(MODIFY_BEARER_REQ) //ignore ret
+		fmt.Printf("MME Unhandled message(%d) received\n", msg.Msg_type)
+		msg.Msg_type = INVALID
+		msg_str, _ := json.Marshal(&msg)
+		return []byte(msg_str)
+	}
+
+	msg.Msg_type = ERROR
+	msg_str, _ := json.Marshal(&msg)
+	return []byte(msg_str)
+}
+func hss_sgw_stub(msg_type S1ap_message_t) int {
+	//If remote hss_sgw_stub, create tcp connection. The response can
+	//sync or async
+	conn, _ := net.Dial("tcp", "128.110.154.116:8081")
+	text := "STB_MSG"
+	fmt.Fprintf(conn, text+"\n")
+	// Remove this for async response from stub
+	_, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error in reading response from hss_sgw_stub")
+		return 1
+	}
+	return 0
+	//TODO: Add appropriate sleep for each message type
+	// switch {
+	// case auth_info_req:
+	// 	return auth_info_answer
+
+	// case update_location_req:
+	// 	return update_location_answer
+
+	// case create_session_req:
+	// 	return create_session_res
+
+	// case modify_bearer_req:
+	// 	return modify_bearer_res
+	// }
+}
+func build_auth_request(id uint64, ue_info Ue_info) []byte {
+	ue_info.Message.Msg_type = AUTH_REQ
+	ue_info.Message.Auth_req.Mme_ue_s1ap_id = ue_info.Mme_ue_s1ap_id
+	ue_info.Message.Auth_req.Enb_ue_s1ap_id = ue_info.Enb_ue_s1ap_id
+	ue_info.Message.Auth_req.Auth_challenge = 0xaa
+	msg_str, _ := json.Marshal(&ue_info.Message)
+	return msg_str
+}
+
+func build_sec_mode_command(id uint64, ue_info Ue_info) []byte {
+
+	ue_info.Message.Msg_type = SEC_MODE_COMMAND
+	ue_info.Message.Sec_mode_command.Mme_ue_s1ap_id = ue_info.Mme_ue_s1ap_id
+	ue_info.Message.Sec_mode_command.Enb_ue_s1ap_id = ue_info.Enb_ue_s1ap_id
+	ue_info.Message.Sec_mode_command.Sec_algo = 0xaa
+	msg_str, _ := json.Marshal(&ue_info.Message)
+	update(id, ue_info)
+	return msg_str
+}
+
+func build_attach_accept(id uint64, ue_info Ue_info) []byte {
+	ue_info.Message.Msg_type = ATTACH_ACCEPT
+	ue_info.Message.Attach_accept.Mme_ue_s1ap_id = ue_info.Mme_ue_s1ap_id
+	ue_info.Message.Attach_accept.Enb_ue_s1ap_id = ue_info.Enb_ue_s1ap_id
+	ue_info.Message.Attach_accept.Ambr = 100
+	ue_info.Message.Attach_accept.Sec_cap = 100
+	msg_str, _ := json.Marshal(&ue_info.Message)
+	update(id, ue_info)
+	return msg_str
+}
