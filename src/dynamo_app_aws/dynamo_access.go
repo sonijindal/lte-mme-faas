e5327e18262e50359134408da80c5a5e7b567f86
sonika05 <sonika05@node.sj-ubuntu-pc.phantomnet-pg0.utah.cloudlab.us>
2019-01-26 21:12:47 -0700

    Change the update item query

--- src/dynamo_app_aws/dynamo_access.go
+++ src/dynamo_app_aws/dynamo_access.go
@@ -113,25 +113,46 @@ func get(id uint64) *Ue_info {
 	return item
 }
 
+type Ue_info_update struct {
+	Info Ue_info `json:":in"`
+}
+
 func update(id uint64, ue_info Ue_info) bool {
-	item := new(Item)
-	item.Key = id
-	item.Info = ue_info
-	av, err := dynamodbattribute.MarshalMap(item)
-	input := &dynamodb.PutItemInput{
-		Item:      av,
+	//var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-east-2"))
+
+	//ue_info.
+	update := Ue_info_update{ue_info}
+
+	update_info, err := dynamodbattribute.MarshalMap(update)
+	if err != nil {
+		fmt.Println("During update, error in MarshalMap:")
+		fmt.Println(err.Error())
+		return false
+	}
+	input := &dynamodb.UpdateItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				N: aws.String(strconv.FormatUint(id, 10)),
+			},
+		},
 		TableName: aws.String("ue_info"),
+
+		ExpressionAttributeNames: map[string]*string{
+			"#IN": aws.String("info"),
+		},
+		ExpressionAttributeValues: update_info,
+
+		UpdateExpression: aws.String("SET #IN = :in"),
 	}
 
-	_, err = db.PutItem(input)
+	_, err = db.UpdateItem(input)
 
 	if err != nil {
-		fmt.Println("Got error calling PutItem:")
+		fmt.Println("During update, got error calling UpdateItem:")
 		fmt.Println(err.Error())
-		os.Exit(1)
+		return false
 	}
 
-	//fmt.Println("Successfully updated")
 	return true
 }
 func generate_mme_id() uint64 {
@@ -150,7 +171,7 @@ func Handle(request events.APIGatewayProxyRequest) (string, error) {
 		id = generate_mme_id()
 	}*/
 	var ue_info Ue_info
-	id := uint64(86939549076244585)
+	id := uint64(533912579193215389)
 	start := time.Now()
 	update(id, ue_info)
 	elapsed := time.Since(start)
